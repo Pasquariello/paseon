d8fff89ba5aeddde1273288a31ffbe98d7a2d8b3
Taylor Pasquariello <taylorpasquariello@Taylors-MacBook-Pro.local>
2019-10-24 16:35:12 -0600

    basic about ideas fleshed out

--- pages/about.js
+++ pages/about.js
@@ -6,7 +6,18 @@ export default function About() {
     <>
       <Layout>
         <p>This is the about page</p>
-      </Layout>
+        Form Submissions for all ocassions
+        <p>Use Paseon for simple forms and form submissions without the need to write your own backend code.</p>
+        <p>Choose from a suit of prebuilt basic forms; Contact forms, Sign Up forms, RSCVP forms.</p>
+        <p>Write your own custom HTML form and nest it instide the Paseon tags to handle all the submission details.</p>
+        <p>Use the Paseon drag and drop tool to build your own custom forms or any other third party form build tool that works best for you.</p>
+      
+        <p>Need to manage multiple form, archive emails, watch submission details, integrate with third party tools like MailChimp, Freshdesk, Jira and more? Create an account  </p>
+     
+     
+        <p>Want to build smart and effectiv marketing campaigns?</p>
+        <p>Paseon sign up forms will let you manage user subscriptions, import user information into third party tools like, Mailchimp, SendGrid, and other drip marketing tools.</p>
+       </Layout>
     </>
   );
 }
