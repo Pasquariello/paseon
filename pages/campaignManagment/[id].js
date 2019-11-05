a3efca0be77c5681486d76bb7a014682bf6fc621
Taylor Pasquariello <taylorpasquariello@Taylors-MacBook-Pro.local>
2019-11-04 18:49:35 -0700

    get campaign  details

--- pages/campaignManagment/[id].js
+++ pages/campaignManagment/[id].js
@@ -5,108 +5,152 @@ import moment from 'moment';
 import CampaignLink from '../../components/CampaignLink';
 import Link from 'next/link';
 
-const Campaign = props => (
+export default function Campaign(props) {
+
+    const renderTable = (fields) => {
+
+        console.log('in columns', fields)
+        // TODO: rename? name columns and make a stateful value? 
+        let build = []
+
+        fields.map(field => {
+            build.push(
+                {
+                Header: field.label,
+                accessor: field.label,
+                Cell: field.value
+                },
+       
+            );
+        });
+
+        //TODO: turn this into its own function that takes in columns? 
+        return (
+            <>
+                <p>Visual representation of data collected with your new campaign.</p>
+                <ReactTable
+                // data={props}
+                columns={build}
+                /> 
+            </>
+        )
+    }
+
+
+return (
   <Layout>
-    <h3>{props.dataOne[0].name}</h3>
+
+    <h3>{props.data[0].name}</h3>
     {/* <h1>{props.show.name}</h1>
     <p>{props.show.summary.replace(/<[/]?p>/g, '')}</p>
     <img src={props.show.image.medium} /> */}
+    {props.data ? renderTable(props.data) : null}
 
 
-    <ReactTable
+    {/* <ReactTable
         data={props.dataOne}
         columns={props.columns}
-    />
+    /> */}
   </Layout>
 );
+}
 
-Campaign.getInitialProps = async function(context) {
-  const {id} = context.query;
-  console.log('id', id)
+Campaign.getInitialProps = async function(context, props) {
+    const {id} = context.query;
+    console.log('my props', props)
+    console.log('id', id)
+    console.log(context)
   // this will need to be a req to DB
-  // const res = await fetch(`https://api.tvmaze.com/shows/${id}`);
-  // const show = await res.json();
+    const res = await fetch(`http://localhost:3001/campaign/get_campaigns/${id}`);
+    const data = await res.json();
+    
+    console.log('data', data);
+    
+    // let  dataOne = data.filter(data => data.id == id)
+    // return { id, dataOne, columns };
+    return {data}
+
  // console.log(`Fetched show: ${show.name}`);
 
   // return { show };
 
   ///START grid faux data
-  const data = [
-    {
-        id: 1,
-        name: 'Elevation Companies Contact Form',
-        recipient_Email: 'taylorpasq@gmail.com',
-        recipient_Email_hashed: hashCode('Taylorpasq@gmail.com'),
-        date_created: moment().format('LLL'),
-        url: 'elevationcompanies.com',
-        form_type: {
-            type : 'basic_contact',
-            display : 'Basic Contact',
-        }
-    },
-    {
-        id: 2,
-        name: 'Maverick Contact Form',
-        recipient_Email: 'maverick@gmail.com',
-        recipient_Email_hashed: hashCode('maverick@gmail.com',),
-        date_created: moment().format('LLL'),
-        url: 'maverickkitchens.com',
-        form_type: {
-            type : 'basic_contact',
-            display : 'Basic Contact'
-        }
-    },
-    {
-        id: 3,
-        name: 'BitterSweet RSVP Form',
-        recipient_Email: 'dine@bittersweet.com',
-        recipient_Email_hashed: hashCode('dine@bittersweet.com',),
-        date_created: moment().format('LLL'),
-        url: 'bittersweetdenver.com/',
-        form_type: {
-            type : 'basic_RSVP',
-            display : 'Basic RSVP'
-        }
-    }
-]
+//   const data = [
+//     {
+//         id: 1,
+//         name: 'Elevation Companies Contact Form',
+//         recipient_Email: 'taylorpasq@gmail.com',
+//         recipient_Email_hashed: hashCode('Taylorpasq@gmail.com'),
+//         date_created: moment().format('LLL'),
+//         url: 'elevationcompanies.com',
+//         form_type: {
+//             type : 'basic_contact',
+//             display : 'Basic Contact',
+//         }
+//     },
+//     {
+//         id: 2,
+//         name: 'Maverick Contact Form',
+//         recipient_Email: 'maverick@gmail.com',
+//         recipient_Email_hashed: hashCode('maverick@gmail.com',),
+//         date_created: moment().format('LLL'),
+//         url: 'maverickkitchens.com',
+//         form_type: {
+//             type : 'basic_contact',
+//             display : 'Basic Contact'
+//         }
+//     },
+//     {
+//         id: 3,
+//         name: 'BitterSweet RSVP Form',
+//         recipient_Email: 'dine@bittersweet.com',
+//         recipient_Email_hashed: hashCode('dine@bittersweet.com',),
+//         date_created: moment().format('LLL'),
+//         url: 'bittersweetdenver.com/',
+//         form_type: {
+//             type : 'basic_RSVP',
+//             display : 'Basic RSVP'
+//         }
+//     }
+// ]
 
 
-const columns = [
-    {
-        id: 'name',
-        Header: 'Name',
-        accessor: 'name',
-        Cell: props => <CampaignLink id={props.original.id} title={props.value}/>
-    }, 
-    {
-        Header: 'Recipient Email',
-        accessor: 'recipient_Email',
-        Cell: props => <span className='number'>{props.value}</span> // Custom cell components!
-    }, 
-    {
-        Header: 'Email Hash',
-        accessor: 'recipient_Email_hashed'
-    },
-    {
-        id: 'form_type', // Required because our accessor is not a string
-        Header: 'Form Type',
-        accessor: d => d.form_type.display, // Custom value accessors!
-    },
-    {
-        Header: 'URL',
-        accessor: 'url',
-        Cell: props =>  <Link prefetch={false} href={`http://${props.value}`}><a target="_blank">{`http://${props.value}`}</a></Link> // Custom cell components!
+// const columns = [
+//     {
+//         id: 'name',
+//         Header: 'Name',
+//         accessor: 'name',
+//         Cell: props => <CampaignLink id={props.original.id} title={props.value}/>
+//     }, 
+//     {
+//         Header: 'Recipient Email',
+//         accessor: 'recipient_Email',
+//         Cell: props => <span className='number'>{props.value}</span> // Custom cell components!
+//     }, 
+//     {
+//         Header: 'Email Hash',
+//         accessor: 'recipient_Email_hashed'
+//     },
+//     {
+//         id: 'form_type', // Required because our accessor is not a string
+//         Header: 'Form Type',
+//         accessor: d => d.form_type.display, // Custom value accessors!
+//     },
+//     {
+//         Header: 'URL',
+//         accessor: 'url',
+//         Cell: props =>  <Link prefetch={false} href={`http://${props.value}`}><a target="_blank">{`http://${props.value}`}</a></Link> // Custom cell components!
 
-    },
-    {
-        Header: 'Date Created',
-        accessor: 'date_created',
-    }
-]
+//     },
+//     {
+//         Header: 'Date Created',
+//         accessor: 'date_created',
+//     }
+// ]
 
-function hashCode(s) {
-  return s.split("").reduce(function(a,b){a=((a<<5)-a)+b.charCodeAt(0);return a&a},0);              
-}
+// function hashCode(s) {
+//   return s.split("").reduce(function(a,b){a=((a<<5)-a)+b.charCodeAt(0);return a&a},0);              
+// }
 ///END grid faux data
 
 
@@ -114,9 +158,6 @@ function hashCode(s) {
 // rename data one! 
 // table columns need to be form fields mapped to columns - make editable?
 // export button
-let  dataOne = data.filter(data => data.id == id)
-  return { id, dataOne, columns };
+
 
 };
-
-export default Campaign;
--- pages/campaignManagment/campaigns.js
+++ pages/campaignManagment/campaigns.js
@@ -5,9 +5,10 @@ import QuickAnalytics from '../../components/QuickAnalytics';
 import ReactTable from 'react-table';
 import moment from 'moment';
 import CampaignLink from '../../components/CampaignLink';
+import fetch from 'isomorphic-unfetch';
 
 
-export default function Campaigns() {
+export default function Campaigns(props) {
     let quickAnalyticsData = [
         {
           title: 'Contact Form',
@@ -20,84 +21,111 @@ export default function Campaigns() {
           title: 'RSVP From',
         },
     ] 
+    
 
-    ///START grid faux data
-    const data = [
-        {
-            id: 1,
-            name: 'Elevation Companies Contact Form',
-            recipient_Email: 'taylorpasq@gmail.com',
-            recipient_Email_hashed: hashCode('Taylorpasq@gmail.com'),
-            date_created: moment().format('LLL'),
-            url: 'elevationcompanies.com',
-            form_type: {
-                type : 'basic_contact',
-                display : 'Basic Contact'
-            }
-        },
-        {
-            id: 2,
-            name: 'Maverick Contact Form',
-            recipient_Email: 'maverick@gmail.com',
-            recipient_Email_hashed: hashCode('maverick@gmail.com',),
-            date_created: moment().format('LLL'),
-            url: 'maverickkitchens.com',
-            form_type: {
-                type : 'basic_contact',
-                display : 'Basic Contact'
-            }
-        },
-        {
-            id: 3,
-            name: 'BitterSweet RSVP Form',
-            recipient_Email: 'dine@bittersweet.com',
-            recipient_Email_hashed: hashCode('dine@bittersweet.com',),
-            date_created: moment().format('LLL'),
-            url: 'bittersweetdenver.com/',
-            form_type: {
-                type : 'basic_RSVP',
-                display : 'Basic RSVP'
-            }
-        }
-    ]
+    const data = props.CampaignList;
+   
 
+    const renderTable = () => {
+    
+        const columns = [
+                {
+                    id: 'name',
+                    Header: 'Name',
+                    accessor: 'campaign_name',
+                    Cell: props => <CampaignLink id={props.original.id} title={props.value}/>
 
-    const columns = [
-        {
-            id: 'name',
-            Header: 'Name',
-            accessor: 'name',
-            Cell: props => <CampaignLink id={props.original.id} title={props.value}/>
-        }, 
-        {
-            Header: 'Recipient Email',
-            accessor: 'recipient_Email',
-            Cell: props => <span className='number'>{props.value}</span> // Custom cell components!
-        }, 
-        {
-            Header: 'Email Hash',
-            accessor: 'recipient_Email_hashed'
-        },
-        {
-            id: 'form_type', // Required because our accessor is not a string
-            Header: 'Form Type',
-            accessor: d => d.form_type.display, // Custom value accessors!
-        },
-        {
-            Header: 'URL',
-            accessor: 'url',
-            Cell: props =>  <Link prefetch={false} href={`http://${props.value}`}><a target="_blank">{`http://${props.value}`}</a></Link> // Custom cell components!
-
-        },
-        {
-            Header: 'Date Created',
-            accessor: 'date_created',
-        }
-    ]
-
-    function hashCode(s) {
-        return s.split("").reduce(function(a,b){a=((a<<5)-a)+b.charCodeAt(0);return a&a},0);              
+                }
+        ]
+        
+        //TODO: turn this into its own function that takes in columns? 
+        return (
+            <>
+            <p>Visual representation of data collected with your new campaign.</p>
+            <ReactTable
+            data={props.CampaignList}
+            columns={columns}
+            /> 
+            </>
+        )
     }
+    ///START grid faux data
+    // const data = [
+    //     {
+    //         id: 1,
+    //         name: 'Elevation Companies Contact Form',
+    //         recipient_Email: 'taylorpasq@gmail.com',
+    //         recipient_Email_hashed: hashCode('Taylorpasq@gmail.com'),
+    //         date_created: moment().format('LLL'),
+    //         url: 'elevationcompanies.com',
+    //         form_type: {
+    //             type : 'basic_contact',
+    //             display : 'Basic Contact'
+    //         }
+    //     },
+    //     {
+    //         id: 2,
+    //         name: 'Maverick Contact Form',
+    //         recipient_Email: 'maverick@gmail.com',
+    //         recipient_Email_hashed: hashCode('maverick@gmail.com',),
+    //         date_created: moment().format('LLL'),
+    //         url: 'maverickkitchens.com',
+    //         form_type: {
+    //             type : 'basic_contact',
+    //             display : 'Basic Contact'
+    //         }
+    //     },
+    //     {
+    //         id: 3,
+    //         name: 'BitterSweet RSVP Form',
+    //         recipient_Email: 'dine@bittersweet.com',
+    //         recipient_Email_hashed: hashCode('dine@bittersweet.com',),
+    //         date_created: moment().format('LLL'),
+    //         url: 'bittersweetdenver.com/',
+    //         form_type: {
+    //             type : 'basic_RSVP',
+    //             display : 'Basic RSVP'
+    //         }
+    //     }
+    // ]
+
+
+    // const columns = [
+    //     {
+    //         id: 'name',
+    //         Header: 'Name',
+    //         accessor: 'name',
+    //         Cell: props => <CampaignLink id={props.original.id} title={props.value}/>
+    //     }, 
+    //     {
+    //         Header: 'Recipient Email',
+    //         accessor: 'recipient_Email',
+    //         Cell: props => <span className='number'>{props.value}</span> // Custom cell components!
+    //     }, 
+    //     {
+    //         Header: 'Email Hash',
+    //         accessor: 'recipient_Email_hashed'
+    //     },
+    //     {
+    //         id: 'form_type', // Required because our accessor is not a string
+    //         Header: 'Form Type',
+    //         accessor: d => d.form_type.display, // Custom value accessors!
+    //     },
+    //     {
+    //         Header: 'URL',
+    //         accessor: 'url',
+    //         Cell: props =>  <Link prefetch={false} href={`http://${props.value}`}><a target="_blank">{`http://${props.value}`}</a></Link> // Custom cell components!
+
+    //     },
+    //     {
+    //         Header: 'Date Created',
+    //         accessor: 'date_created',
+    //     }
+    // ]
+
+    // function hashCode(s) {
+    //     return s.split("").reduce(function(a,b){a=((a<<5)-a)+b.charCodeAt(0);return a&a},0);              
+    // }
     ///END grid faux data
 
     return (
@@ -123,10 +151,12 @@ export default function Campaigns() {
                 </Link>
             </div>
             <div style={{marginTop: '20px'}}>
-            <ReactTable
+            {props.CampaignList.length ? renderTable() : null}
+
+            {/* <ReactTable
                 data={data}
                 columns={columns}
-            />
+            /> */}
             </div>
             
 
@@ -135,4 +165,19 @@ export default function Campaigns() {
         </LayoutApp>
 
     );
+}
+
+Campaigns.getInitialProps = async function(context) {
+    console.log(
+        'hello from init props'
+    )
+    const res = await fetch('http://localhost:3001/campaign/get_campaigns');
+    const data = await res.json();
+    console.log('resolved');
+
+    console.log('I got: ',data);
+  
+    return {
+      CampaignList: data
+    };
 }
