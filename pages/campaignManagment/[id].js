e2867e47adce546d7d874b162786d6fc37bd4ea4
Taylor <taylor@pasq.net>
2019-11-13 01:28:35 -0700

    playing with new data structure with id 138

--- pages/campaignManagment/[id].js
+++ pages/campaignManagment/[id].js
@@ -13,6 +13,7 @@ import { useState } from 'react';
 export default function Campaign(props) {
 
 
+
     const renderTable = (fields) => {
         
         const columns = [];
@@ -20,14 +21,54 @@ export default function Campaign(props) {
         let myArray = props.data.form_data.map(obj =>{
             return obj.field_values
         })
+
+        console.log('props.data.form_data', props.data.form_data)
+
+        console.log('props.data.data_schema[0]', props.data.data_schema[0])
+
         
-        const columnData = props.data.data_schema[0].schema.map((key, i)=>{
+        //NEEED THIS FOR IT TO WORK! - todo add schema to DB ?  
+        // - keep the schema column we have in order to preserve the form make a new column for schema to look like this
+        let myschema = {
+            name: {
+                label: 'name',
+                value: ''
+            },
+            age:{
+                label: 'age',
+                value: ''
+            },
+
+        }
+
+        const mykeys = Object.keys(myschema).map(key => key)
+console.log(mykeys)
+
+        const columnData = Object.keys(myschema).map((key, i)=>{
+            console.log(key)
+            console.log(i)
+
             columns.push({
                 id: `${i}`,
-                Header: d => key.label,
-                accessor: d => d.field_values[i].value
-            });
+                Header: key,//d => {console.log('key', key) ,key },
+                accessor: props => props[key].value
+                 //{console.log('barley', props)}
+                //d => d.field_values[0][key].value, //console.log('bear',d), console.log('bear 2',d.field_values[i].name.value),
+            }); //d.field_values[0][key].value
         });
+
+
+        
+  
+        
+        // const columnData = props.data.data_schema[0].schema.map((key, i)=>{
+        //     columns.push({
+        //         id: `${i}`,
+        //         Header: d => key.label,
+        //         //accessor: d => d.field_values[i].value
+        //     });
+        // });
+    
 
         const headerData = props.data.data_schema[0].schema.map((item, i)=>{
            
@@ -64,12 +105,71 @@ export default function Campaign(props) {
           
                 <ReactTable
                     columns={columns}
-                    data={props.data.form_data}
+                    data={ props.data.form_data[0].field_values}
                     resolveData={data => data.map(row => row)}
                 /> 
             </>
         )
     }
+
+
+//     const renderTable = (fields) => {
+        
+//         const columns = [];
+//         const headers = [];
+//         let myArray = props.data.form_data.map(obj =>{
+//             return obj.field_values
+//         })
+        
+//         const columnData = props.data.data_schema[0].schema.map((key, i)=>{
+//             columns.push({
+//                 id: `${i}`,
+//                 Header: d => key.label,
+//                 accessor: d => d.field_values[i].value
+//             });
+//         });
+
+//         const headerData = props.data.data_schema[0].schema.map((item, i)=>{
+           
+//             return {
+//                 label: item.label,
+//                 key: 'value'
+//             }
+            
+//         })
+
+
+//   //TODO: turn this into its own function that takes in columns? 
+//     // kind of works data={props.data.form_data.map((data, i) => data.field_values.map(obj => obj.value))}
+//     let csvArray = []
+//     let csvData = props.data.form_data.map((data, i) => data.field_values.map(obj => {csvArray.push(obj)}))
+//     // let csvData = props.data.form_data.map((data, i) => csvArray.push(data.field_values))
+
+
+
+//     console.log(csvArray)
+//         return (
+//             <>
+//             <CSVLink
+//                 data={csvArray}
+//                 // data={props.data.form_data.map((data, i) => data.field_values.map(obj => {obj}))}
+
+//                 onClick={() => {
+//                 console.log("You click the link");
+//                 }}
+//                 >
+//                 Download me
+//             </CSVLink>
+
+          
+//                 <ReactTable
+//                     columns={columns}
+//                     data={props.data.form_data}
+//                     resolveData={data => data.map(row => row)}
+//                 /> 
+//             </>
+//         )
+//     }
     
 
 
