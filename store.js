278d4285e759f8ce381af5f42875723f957f056f
Taylor <taylor@pasq.net>
2019-11-17 12:50:35 -0700

    add thunk set up auth reducer and auth actions

--- store.js
+++ store.js
@@ -1,51 +1,76 @@
 import { createStore, applyMiddleware } from 'redux'
 import { composeWithDevTools } from 'redux-devtools-extension'
+import thunk from "redux-thunk";
+import rootReducer from "./redux/reducers";
 
-const initialState = {
-  lastUpdate: 0,
-  light: false,
-  count: 0,
-  current_user: null
-}
 
-const reducer = (state = initialState, action) => {
-  switch (action.type) {
-    case 'TICK':
-      return {
-        ...state,
-        lastUpdate: action.lastUpdate,
-        light: !!action.light
-      }
-    case 'INCREMENT':
-      return {
-        ...state,
-        count: state.count + 1
-      }
-    case 'DECREMENT':
-      return {
-        ...state,
-        count: state.count - 1
-      }
-    case 'RESET':
-      return {
-        ...state,
-        count: initialState.count
-      }
-    case 'LOGIN':
-      console.log('signed in called')
-      return {
-        ...state,
-        current_user: initialState.current_user
-      }
-    default:
-      return state
-  }
-}
+
+const initialState = {};
+
+const middleware = [thunk];
+
+// const store = createStore(
+//   rootReducer,
+//   initialState,
+//   composeWithDevTools(applyMiddleware(...middleware))
+// );
 
 export const initializeStore = (preloadedState = initialState) => {
   return createStore(
-    reducer,
+    rootReducer,
     preloadedState,
     composeWithDevTools(applyMiddleware())
   )
 }
+
+//export default store;
+
+// const initialState = {
+//   // lastUpdate: 0,
+//   // light: false,
+//   // count: 0,
+//   // isAuthenticated: null,
+//   // current_user: null
+// }
+
+// const reducer = (state = initialState, action) => {
+//   switch (action.type) {
+//     case 'TICK':
+//       return {
+//         ...state,
+//         lastUpdate: action.lastUpdate,
+//         light: !!action.light
+//       }
+//     case 'INCREMENT':
+//       return {
+//         ...state,
+//         count: state.count + 1
+//       }
+//     case 'DECREMENT':
+//       return {
+//         ...state,
+//         count: state.count - 1
+//       }
+//     case 'RESET':
+//       return {
+//         ...state,
+//         count: initialState.count
+//       }
+//     case 'LOGIN':
+//       console.log('signed in called')
+//       return {
+//         ...state,
+//         current_user: initialState.current_user
+//       }
+//     default:
+//       return state
+//   }
+// }
+
+// export const initializeStore = (preloadedState = initialState) => {
+//   return createStore(
+//     reducer,
+//     preloadedState,
+//     composeWithDevTools(applyMiddleware())
+//   )
+// }
