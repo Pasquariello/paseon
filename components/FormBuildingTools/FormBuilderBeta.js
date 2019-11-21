833acf947f23ef382ef472fd973e38ea3c25b0d0
Taylor Pasquariello <taylorpasq@gmail.com>
2019-11-21 12:09:44 -0700

    update html5 syntax to JSX for tabIndex

--- components/FormBuildingTools/FormBuilderBeta.js
+++ components/FormBuildingTools/FormBuilderBeta.js
@@ -700,7 +700,7 @@ let lastElem = campaignForm.fields.length - 1
 
 
 {/* <!-- Modal --> */}
-<div className="modal fade" id="exampleModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+<div className="modal fade" id="exampleModal" tabIndex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
   <div className="modal-dialog" role="document">
     <div className="modal-content">
       <div className="modal-header">
