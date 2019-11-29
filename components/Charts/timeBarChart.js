d14f3770ef215c95a3d851006e48b672016ebe37
Taylor <taylor@pasq.net>
2019-11-29 08:20:24 -0600

    update victory graph y coordinate

--- components/Charts/timeBarChart.js
+++ components/Charts/timeBarChart.js
@@ -33,11 +33,11 @@ class TimeBarChart extends React.Component {
       data: { fill: "#c43a31" }
     }}
     data={[
-        { x: 1, y: 2, y0: 1 },
-        { x: 2, y: 3, y0: 2 },
-        { x: 3, y: 5, y0: 2 },
-        { x: 4, y: 4, y0: 3 },
-        { x: 5, y: 6, y0: 3 }
+        { x: 1, y: 2, y0: 0 },
+        { x: 2, y: 3, y0: 0 },
+        { x: 3, y: 5, y0: 0 },
+        { x: 4, y: 4, y0: 0 },
+        { x: 5, y: 6, y0: 0 }
       ]}  />
 </VictoryChart>
 
