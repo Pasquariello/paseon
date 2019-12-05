a8e25bc98270625d2a8e34c28f9a1348a0222ee5
Taylor Pasquariello <taylorpasq@gmail.com>
2019-12-05 16:26:27 -0700

    format account form HTML

--- pages/account.js
+++ pages/account.js
@@ -45,37 +45,64 @@ import React, { useState } from 'react'
                 <form>
                     <div className="form-row">
                         <div className="form-group col-md-6">
-                        <label for="inputEmail4">Email</label>
-                        <input value={userState.email} type="email" className="form-control" id="inputEmail4" placeholder="Email"></input>
+                        <label htmlFor="inputEmail4">Email</label>
+                        <input 
+                            value={userState.email} 
+                            type="email" 
+                            className="form-control" 
+                            id="inputEmail4" 
+                            placeholder="Email"></input>
                         </div>
                         <div className="form-group col-md-6">
-                        <label for="inputPassword4">Password</label>
-                        <input type="password" className="form-control" id="inputPassword4" placeholder="Password"></input>
+                        <label htmlFor="inputPassword4">Password</label>
+                        <input 
+                            type="password" 
+                            className="form-control" 
+                            id="inputPassword4" 
+                            placeholder="Password"></input>
                         </div>
                     </div>
                     <div className="form-group">
-                        <label for="inputCompany">Company</label>
-                        <input value={userState.company}  type="text" className="form-control" id="inputCompany" placeholder="Company"></input>
+                        <label htmlFor="inputCompany">Company</label>
+                        <input 
+                            value={userState.company}  
+                            type="text" 
+                            className="form-control" 
+                            id="inputCompany" 
+                            placeholder="Company"></input>
                     </div>
                     <div className="form-group">
-                        <label for="inputAddress2">Address 2</label>
-                        <input type="text" className="form-control" id="inputAddress2" placeholder="Apartment, studio, or floor"></input>
+                        <label htmlFor="inputAddress2">Address 2</label>
+                        <input 
+                            type="text" 
+                            className="form-control" 
+                            id="inputAddress2" 
+                            placeholder="Apartment, studio, or floor"></input>
                     </div>
                     <div className="form-row">
                         <div className="form-group col-md-6">
-                        <label for="inputCity">City</label>
-                        <input type="text" className="form-control" id="inputCity"></input>
+                        <label htmlFor="inputCity">City</label>
+                        <input 
+                            type="text" 
+                            className="form-control" 
+                            id="inputCity"></input>
                         </div>
                         <div className="form-group col-md-4">
-                        <label for="inputState">State</label>
-                        <select id="inputState" defaultValue="choose" className="form-control">
-                            <option value="choose">Choose...</option>
-                            <option>...</option>
+                        <label htmlFor="inputState">State</label>
+                        <select 
+                            id="inputState" 
+                            defaultValue="choose" 
+                            className="form-control">
+                                <option value="choose">Choose...</option>
+                                <option>...</option>
                         </select>
                         </div>
                         <div className="form-group col-md-2">
-                        <label for="inputZip">Zip</label>
-                        <input type="text" className="form-control" id="inputZip"></input>
+                        <label htmlFor="inputZip">Zip</label>
+                        <input 
+                            type="text" 
+                            className="form-control" 
+                            id="inputZip"></input>
                         </div>
                     </div>
                 </form>
