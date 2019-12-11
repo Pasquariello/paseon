35e82981f4f2dc6c7cd2ee503c0e6707aaa9e504
Taylor <taylor@pasq.net>
2019-12-10 23:53:17 -0700

    add nprogess

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,15 @@
 import React from "react";
 import App from "next/app";
 import Head from "next/head";
+import NProgress from 'nprogress'
+import Router from 'next/router'
 
+Router.events.on('routeChangeStart', url => {
+  console.log(`Loading: ${url}`)
+  NProgress.start()
+})
+Router.events.on('routeChangeComplete', () => NProgress.done())
+Router.events.on('routeChangeError', () => NProgress.done())
 class MyApp extends App {
   render() {
     const { Component, pageProps } = this.props;
@@ -9,6 +17,8 @@ class MyApp extends App {
       <>
         <Head>
           <script src="https://js.stripe.com/v3/" />
+          <link rel="stylesheet" type="text/css" href="/static/css/nprogress.css" />
+
           <title>
             Paseon
           </title>
