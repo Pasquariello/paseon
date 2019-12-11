0e84bffca00a5753432bcd5182bada04700906c3
Taylor <taylor@pasq.net>
2019-12-10 23:11:39 -0700

    fix broken dynamic route

--- components/CampaignLink.js
+++ components/CampaignLink.js
@@ -3,7 +3,7 @@ import Link from 'next/link';
 
 const CampaignLink = props => (
 
-    <Link href={`/${props.directory}[id]`}  as={`/${props.directory}/${props.id}`}>
+    <Link href={`/${props.directory}/[id]`}  as={`/${props.directory}/${props.id}`}>
     <a>{props.title}</a>
     {/* "/campaignManagment/[id]" */}
     </Link>
