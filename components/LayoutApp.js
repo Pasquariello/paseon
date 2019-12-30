1cd1093caeb82b3a4f5516e59ebf28d4990ac3ae
Taylor <taylor@pasq.net>
2019-12-30 01:08:13 -0700

    add idea to help mobile double click bug

--- components/LayoutApp.js
+++ components/LayoutApp.js
@@ -48,6 +48,7 @@ return (
         {/* </Head> */}
           <div
             onMouseEnter={()=> setHoverState(true)} onMouseLeave={()=> setHoverState(false)}
+            // onClick={()=> toggleState()} // Need onClick for mobile view!
             className={`${hoverState ?  'sidenav-active' : 'sidenav-hidden'} sidenav border-right`}
           >
             <SideBar
