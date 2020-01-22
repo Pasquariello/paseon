7c69cd2d18104d1c2e6e134c117baf74fa65d024
Taylor <taylor@pasq.net>
2020-01-22 00:40:26 -0700

    update gitignore

--- .next/static/runtime/webpack.js
+++ .next/static/runtime/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "96a0a4cd65caff419d78";
+/******/ 	var hotCurrentHash = "52379f22ded1ee681fd7";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
